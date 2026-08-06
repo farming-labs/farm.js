9fb9b59a83b014eb72ccc040ab64ad01a3047a37
Kinfe123 <kinfishtech@gmail.com>
2026-08-06 13:53:57 +0300

    refine docs sidebar connector junction

--- packages/farm/src/__tests__/docs-handler.test.ts
+++ packages/farm/src/__tests__/docs-handler.test.ts
@@ -342,13 +342,13 @@ describe("createFarmDocsHandler", () => {
       ".sidebar-folder-content > a[data-active]::after, .sidebar-subgroup-title::after, .sidebar-subgroup-content a[data-active]::after",
     );
     expect(html).toContain(
-      ".sidebar-folder-content > a[data-active]::after { left: calc(var(--fd-sidebar-guide-x) + var(--fd-sidebar-branch-gap) + 1px);",
+      ".sidebar-folder-content > a[data-active]::after { left: calc(var(--fd-sidebar-guide-x) + var(--fd-sidebar-branch-gap) + 0.5px);",
     );
     expect(html).toContain(
-      ".sidebar-subgroup-title::after { left: calc(var(--fd-sidebar-guide-x) + var(--fd-sidebar-branch-gap) + 1px); width: calc(var(--fd-sidebar-sub-guide-x) - var(--fd-sidebar-guide-x) - var(--fd-sidebar-branch-gap) - 1px);",
+      ".sidebar-subgroup-title::after { left: calc(var(--fd-sidebar-guide-x) + var(--fd-sidebar-branch-gap) + 0.5px); width: calc(var(--fd-sidebar-sub-guide-x) - var(--fd-sidebar-guide-x) - var(--fd-sidebar-branch-gap) - 0.5px);",
     );
     expect(html).toContain(
-      ".sidebar-subgroup-content a[data-active]::after { left: calc(var(--fd-sidebar-sub-guide-x) + 1px);",
+      ".sidebar-subgroup-content a[data-active]::after { left: calc(var(--fd-sidebar-sub-guide-x) + 0.5px);",
     );
     expect(html).toContain(
       "--fd-sidebar-sub-guide-x: calc(var(--fd-sidebar-link-x) + 7px); --fd-sidebar-sub-link-x: calc(var(--fd-sidebar-sub-guide-x) + 28px);",
