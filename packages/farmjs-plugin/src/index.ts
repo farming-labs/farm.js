31bf0257e8298da1d8b2f545f2e73d3c14fcada7
Kinfe123 <kinfishtech@gmail.com>
2026-08-07 23:14:05 +0300

    chore: release v0.1.0-beta.21

--- packages/farmjs-plugin/src/index.ts
+++ packages/farmjs-plugin/src/index.ts
@@ -40,7 +40,7 @@
  * ```
  */
 
-export const version = "0.1.0-beta.20";
+export const version = "0.1.0-beta.21";
 
 // Note: Import plugins from their respective subpaths:
 // - @farm.js/plugin/rsc
